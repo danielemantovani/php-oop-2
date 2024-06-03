6c7d9478d405e0960428b74a1ba2ae831dfb308a
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-03 17:06:14 +0200

    fix: product

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
-require_once __DIR__ . "/models/products.php";
+require_once __DIR__ . "/models/product.php";
 
+$product1 = new Product("Ciotola", 25, "ciotola.png");
+$product2 = new Product("Fieno", 4,50, "fieno.png");
+
+var_dump($product1);
+var_dump($product2);
 
 
 ?>
