a7917ae187c0b971835809a642b6a80badf84d4f
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-04 23:14:36 +0200

    feat: food

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . "/models/product.php";
 require_once __DIR__ . "/models/category.php";
+require_once __DIR__ . "/models/food.php";
 
 $rabbitCategory = new Category("Rabbit");
 $rabbitCategory -> setIcon("fa-solid fa-dragon");
@@ -9,8 +10,12 @@ $rabbitCategory -> setIcon("fa-solid fa-dragon");
 $dogCategory = new Category("Dog");
 $dogCategory -> setIcon("fa-solid fa-paw");
 
+$foodType = new Food ("Royal Canini", 18.99, 800);
+
 var_dump($rabbitCategory);
 var_dump($dogCategory);
+
+var_dump($foodType);
 
 
 ?>
--- models/food.php
+++ models/food.php
@@ -1,1 +1,42 @@
 <?php
+
+require_once __DIR__ . "/product.php";
+
+class Food extends Product {
+    private string $taste;
+    private int $weight;
+
+    public function __construct(string $_name, float $_price, int $_weight) {
+        parent::__construct($_name, $_price);
+
+        $this -> weight = $_weight;
+    }
+
+    /**
+     * Get the value of weightGr
+     *
+     * @return int
+     */
+    public function getWeightGr(): int {
+        return $this->weight;
+    }
+
+    /**
+     * Get the value of taste
+     *
+     * @return string
+     */
+    public function getTaste(): string {
+        return $this->taste;
+    }
+
+    /**
+     * Set the value of taste
+     *
+     * @param string $taste
+     *
+     */
+    public function setTaste(string $_taste): void {
+        $this->taste = $_taste;
+    }
+}
